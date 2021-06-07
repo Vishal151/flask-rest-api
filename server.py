d50c6227e21638a7b35b4b40675dfd67da00e621
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-06-07 20:07:48 +0100

    Testing swagger ui

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import render_template
 import connexion
 
-app = connexion.App(__name__, specification_dir='./')
+app = connexion.App(__name__, specification_dir="./")
 
-app.add_api('swagger.yml')
+# read the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
 
 @app.route("/")
 def home():
   return render_template("home.html")
 
-if (__name__=="__main__"):
-  app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
